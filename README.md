3c8d1a853260615874aa19aa7977e8bbc8c91de4
Ioannis Mourginakis <ioannis.mourginakis@gmail.com>
2025-06-13 09:21:59 -0700

    get cudf working

--- README.md
+++ README.md
@@ -12,9 +12,21 @@ python3 -m pip install -r requirements.txt
 ```
 
 If you have vscode/cursor you can also run this as a notebook (recommended).
+(You need to install the Microsoft Jupyter extension)
 
 I built this so that it should work out of the box. We've downloaded the kaggle dataset into the repository and truncated and compressed it so it's at a size that's friendly for git.
 
+
+```bash
+# you need an nvidia GPU with cuda
+nvidia-smi
+
+# you may have to run this:
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
+sudo dpkg -i cuda-keyring_1.1-1_all.deb
+sudo apt-get update
+sudo apt-get install cuda-toolkit-12-4
+```
 
 ### Notes
 
--- main.py
+++ main.py
@@ -1,1 +1,54 @@
-print("I don't do much.... yet!")
+#%% ==================== Imports ====================
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import cudf, cupy, numpy as np, os
+
+# ---------- smoke test ----------
+print(f"cuDF version: {cudf.__version__}")
+print(f"GPU: {cupy.cuda.runtime.getDeviceProperties(0)['name'].decode()}")
+gdf = cudf.DataFrame({"x": cupy.arange(10)})
+print(gdf.sum())
+
+
+# %% ==================== Load Data ====================
+
+# ---------- load data (CPU) ----------
+df = pd.read_csv("btc_usd.csv.xz", compression="xz")
+df.info(memory_usage="deep")
+df.set_index("Timestamp", inplace=True)
+df.index = pd.to_datetime(df.index, unit="s")
+
+# ---------- load data (GPU) ----------
+# cudf does not support xz, so load from cpu dataframe
+gdf = cudf.DataFrame(df)
+# gdf.info(memory_usage="deep")
+
+
+#%% ==================== Get GPU working ====================
+import timeit
+
+
+def gpu_sum():
+    return gdf["Close"].sum()
+
+def cpu_sum(): 
+    return df["Close"].sum()
+
+
+gpu_result = gpu_sum()
+cpu_result = cpu_sum()
+
+assert (gpu_result - cpu_result) < 1e-6, "GPU and CPU results do not match"
+
+print(f"GPU result: {gpu_result}")
+print(f"CPU result: {cpu_result}")
+
+
+cpu_time = timeit.timeit(cpu_sum, number=1_000)
+gpu_time = timeit.timeit(gpu_sum, number=1_000)
+
+print(f"CPU: 100 runs took {cpu_time}, average {cpu_time/100} seconds per run")
+print(f"GPU: 100 runs took {gpu_time}, average {gpu_time/100} seconds per run")
+
