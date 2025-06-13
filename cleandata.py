6bd942c7d0354457463462d298f0e59c3b4a7012
Ioannis Mourginakis <ioannis.mourginakis@gmail.com>
2025-06-13 01:42:19 -0700

    truncate based on time

--- cleandata.py
+++ cleandata.py
@@ -6,14 +6,21 @@ import pandas as pd
 # downloaded data from kaggle (~300mb)
 # https://www.kaggle.com/datasets/mczielinski/bitcoin-historical-data
 df = pd.read_csv("btcusd_1-min_data.csv")
-df.info(memory_usage="deep")
+print(df.info(memory_usage="deep"), end="\n\n")
 
 
 #%% ==================== Clean Data ====================
 # keep only timestamp, close
 # reduces size from 369mb to 107mb
 df1 = df[["Timestamp", "Close"]]
-df1.info(memory_usage="deep")
+print(df1.info(memory_usage="deep"), end="\n\n")
+
+
+# Data is from 2012 till present
+# Truncate from 2017 to 2025
+df1.index = pd.to_datetime(df1["Timestamp"], unit='s')
+df2 = df1.loc["2017-01-01":"2025-01-01"]
+print(df2.info(memory_usage="deep"), end="\n\n")
 
 
 #%% ==================== Export Data ====================
@@ -25,8 +32,8 @@ df1.info(memory_usage="deep")
 # .xz gives really good compression
 # .xz will be committed to the repo, .csv will not.
 
-df1.to_csv("btc_usd.csv", index=False)
-df1.to_csv("btc_usd.csv.xz", index=False, compression="xz")
+df2.to_csv("btc_usd.csv", index=False)
+df2.to_csv("btc_usd.csv.xz", index=False, compression="xz")
 
 
 # %%
